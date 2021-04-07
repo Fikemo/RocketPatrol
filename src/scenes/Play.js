684e97da24ece905435b3f90f3275f43e36bb8b3
Finn Morrison <fmorriso@ucsc.edu>
2021-04-07 16:58:17 -0700

    Rocket Implemented

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,7 +3,35 @@ class Play extends Phaser.Scene {
         super("playScene");
     }
 
+    preload() {
+        // load images/tile sprites
+        this.load.image("rocket", "./assets/rocket.png");
+        this.load.image("spaceship", "./assets/spaceship.png");
+        this.load.image("starfield", "./assets/starfield.png");
+    }
+
     create() {
-        this.add.text(20, 20, "ROCKET PATROL PLAY");
+        this.starfield = this.add.tileSprite(0,0,game.config.width,game.config.height, 'starfield').setOrigin(0,0);
+
+        // this.add.text(20, 20, "ROCKET PATROL PLAY");
+        // green UI background
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0,0);
+
+        // white borders
+        // top
+        this.add.rectangle(0,0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0,0);
+        // bottom
+        this.add.rectangle(0,game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0,0);
+        // left
+        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
+        //right
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
+
+        // add rocket (player 1)
+        this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5,0);
+    }
+
+    update() {
+        this.starfield.tilePositionX -= starSpeed;
     }
 }
